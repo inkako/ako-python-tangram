a1d42b8714030b71b98ef6dd55bd269e028999a8
wuchenghui <W0nder!@>
2026-09-11 20:18:08 +0800

    init

--- src/app/api/v1/__init__.py
+++ src/app/api/v1/__init__.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 
 from app.modules.demo.demo_router import router as demo_router
 
-router = APIRouter(prefix="/v1")
+router = APIRouter(prefix="/v1", tags=["DEMO"])
 router.include_router(demo_router)
